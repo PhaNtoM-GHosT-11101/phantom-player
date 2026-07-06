abd46b8160efba1d9c0e7f2f601152b1d36905c5
Aditya Priyadarshi <aditya.on.windows10@gmail.com>
2026-07-07 01:22:57 +0530

    Build: add 'music' alias to console scripts

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
+            "music=phantom_player.__main__:main",
             "phantom=phantom_player.__main__:main",
             "phantom-player=phantom_player.__main__:main"
         ],
